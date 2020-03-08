68dbdd9affaf7605d977201b5d7bb990898e2322
Sally Ragab <sallyragab31@gmail.com>
2020-03-09 01:47:16 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,56 +1,50 @@
 // validate email address
-const email = document.getElementById('email');
+var email = document.getElementById('email');
 
-function validation() {
+function validateEmail() {
   var emailValue = document.getElementById('email').value;
   var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
   if (emailValue.match(pattern)) {
     email.classList.remove("is-invalid");
   } else {
     email.classList.add("is-invalid");
-
   }
 }
-email.addEventListener('keydown', validation;
+
+email.addEventListener('keydown', validateEmail);
+
+
+// // validate password
+var password = document.getElementById('password');
+
+function validatePassword() {
+  var passwordValue = document.getElementById('password').value;
+  if (passwordValue.length >= 7) {
+    password.classList.remove("is-invalid");
+  } else {
+    password.classList.add("is-invalid");
+  }
+}
+
+password.addEventListener('keydown', validatePassword);
+
 
 // Change color of button when form is filled
-var forms = document.querySelectorAll('input');
-var button = document.getElementById('button');
-var list = [];
+var inputFields = document.querySelectorAll('input');
+
 function changeColor(){
-  for (var i = 0; i < forms.length; i++) {
-      var formValue = forms[i].value;
-      list.push(formValue);
+  var button = document.getElementById('button');
+  var list = [];
+  for (var i = 0; i < inputFields.length; i++) {
+      var inputValue = inputFields[i].value;
+      list.push(inputValue);
   }
-  if (list not includes('')) {
+  if (!(list.includes('')) && !(email.classList.contains("is-invalid")) &&  !(password.classList.contains("is-invalid"))) {
     button.classList.add('active-btn');
+  } else {
+    button.classList.remove('active-btn');
   }
 }
-input.addEventListener('input', changeColor);
 
-// const name = document.getElementById('name').value;
-// const nameValue = document.getElementById('name');
-// // const email = document.getElementById('email').value;
-// // const option = document.getElementById('optio ').value;
-// // const password = document.getElementById('password').value;
-// //
-// //
-// nameValue.addEventListener('input', changeColor);
-// // email.addEventListener('input', changeColor);
-// // option.addEventListener('input', changeColor);
-// // password.addEventListener('input', changeColor);
-// //
-// function changeColor() {
-// //   //var inputValue = document.querySelectorAll('input').value;
-//   var button = document.getElementById('button');
-// //   // let email = document.getElementById('email').value;
-// //   // let option = document.getElementById('option').value;
-// //   // let password = document.getElementById('password').value;
-//   if ((name == ""){
-//      return true;
-//   } else {
-//     button.classList.add('active-btn');
-//   }}
-//
-//  // && (email == "") && (option == "") && (password == ""))
+for (var i = 0; i < inputFields.length; i++) {
+      inputFields[i].addEventListener('input', changeColor);}
